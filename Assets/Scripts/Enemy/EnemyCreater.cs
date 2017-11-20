a509dbee4039484448a5b28476f3db8a14edc98a
sun <sun@sunui-MacBook-Pro.local>
2017-11-20 20:40:01 +0900

    Start to work about enemy

---
+++ Assets/Scripts/Enemy/EnemyCreater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCreater : MonoBehaviour {
+
+	public float speed = 0.0001f;
+	public Transform spaceshipTransform;
+	public GameObject enemyPrefab;
+
+	void Start () {
+		
+		GameObject temp = Instantiate(enemyPrefab);
+
+		//set position
+		temp.transform.position = spaceshipTransform.position;
+
+		//set velocity
+		Vector3 initialVelocity = new Vector3 (Random.Range (-10.0f, 10.0f), Random.Range (-10.0f, 10.0f), -1);
+		initialVelocity.Normalize();
+		initialVelocity *= speed;
+        
+		temp.GetComponent<Rigidbody> ().velocity = initialVelocity;
+		Debug.Log ("initial velocity: " + temp.GetComponent<Rigidbody> ().velocity);
+	}
+
+	void Update () {
+	
+	}
+}
+
---
+++ Assets/Scripts/Enemy/VelocityCalculator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityCalculator : MonoBehaviour {
+
+	Transform targetTransform;
+
+	public float speed = 0.0001f;
+	public float deltaVelocityDir = 0.01f;
+
+	Vector3 curPosition;
+	Vector3 curDir;
+	Vector3 towardDir;
+	Vector3 updatedDir;
+
+	void Start () {
+		targetTransform = GameObject.Find ("Single_Rose").transform;
+	}
+
+	void Update () {
+
+		//get the current position
+		curPosition = gameObject.transform.position;
+		Debug.Log (curPosition);
+
+		//get the current velocity
+		curDir = gameObject.GetComponent<Rigidbody> ().velocity;
+		curDir.Normalize ();
+		Debug.Log ("current dir: " + curDir);
+
+		//get the toward velocity
+		towardDir = targetTransform.position - curPosition;
+		towardDir.Normalize();
+		Debug.Log ("toward dir: " + towardDir);
+
+		//interpolate two vectors
+		updatedDir = Vector3.Lerp(curDir, towardDir, 1.0f);
+		updatedDir.Normalize ();
+		Debug.Log ("updated dir: " + updatedDir);
+		Debug.Log ("delta position: " + updatedDir*speed*Time.deltaTime);
+
+		//change the position
+		gameObject.GetComponent<Rigidbody>().velocity = updatedDir*speed;
+		gameObject.transform.position = curPosition + updatedDir * speed * Time.deltaTime;
+	}
+}
