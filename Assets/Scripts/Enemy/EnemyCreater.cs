201505046ec4d0c965dfd9e64e0f471d22a58d06
DESKTOP-P876KOJ\sun <iamsun0720@gmail.com>
2017-11-23 15:35:08 +0900

    Enemy- Trajectory, pooling system implemented

--- Assets/Scripts/Enemy/EnemyCreater.cs
+++ Assets/Scripts/Enemy/EnemyCreater.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 
 public class EnemyCreater : MonoBehaviour {
 
-	public float speed = 0.0001f;
-	public Transform spaceshipTransform;
-	public GameObject enemyPrefab;
+    public EnemyPool pool;
 
-	void Start () {
-		
-		GameObject temp = Instantiate(enemyPrefab);
+    private float power = 10.0f;
+	public Transform FirePositionTransform;
+    private GameObject temp;
 
-		//set position
-		temp.transform.position = spaceshipTransform.position;
-
-		//set velocity
-		Vector3 initialVelocity = new Vector3 (Random.Range (-10.0f, 10.0f), Random.Range (-10.0f, 10.0f), -1);
-		initialVelocity.Normalize();
-		initialVelocity *= speed;
-        
-		temp.GetComponent<Rigidbody> ().velocity = initialVelocity;
-		Debug.Log ("initial velocity: " + temp.GetComponent<Rigidbody> ().velocity);
-	}
+    private float timeInterval = 0.0f;
+    private float time = 0.0f;
+    
 
 	void Update () {
-	
+
+        if (time >= timeInterval)
+        {   
+            time = 0;
+            timeInterval = Random.Range(0.5f, 1.5f);
+       
+            Shoot();
+        }
+        else
+            time += Time.deltaTime;    
 	}
+
+    private void Shoot()
+    {
+        temp = pool.GetObject();
+
+        //set position
+        temp.transform.position = FirePositionTransform.position;
+
+        //set velocity
+        Vector3 initialVelocity = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0);
+        initialVelocity.Normalize();
+        initialVelocity *= power;
+
+        temp.GetComponent<Rigidbody>().velocity = initialVelocity;
+    }
 }
 
--- Assets/Scripts/Enemy/VelocityCalculator.cs
+++ Assets/Scripts/Enemy/VelocityCalculator.cs
@@ -6,8 +6,11 @@ public class VelocityCalculator : MonoBehaviour {
 
 	Transform targetTransform;
 
-	public float speed = 0.0001f;
-	public float deltaVelocityDir = 0.01f;
+	public float speed;
+    public float lerpValue;
+
+    private Rigidbody rb;
+    private float thrust = 10.0f;
 
 	Vector3 curPosition;
 	Vector3 curDir;
@@ -16,32 +19,39 @@ public class VelocityCalculator : MonoBehaviour {
 
 	void Start () {
 		targetTransform = GameObject.Find ("Single_Rose").transform;
+        rb = GetComponent<Rigidbody>();
 	}
 
 	void Update () {
 
-		//get the current position
-		curPosition = gameObject.transform.position;
-		Debug.Log (curPosition);
+        /*
+        //get the current position
+        curPosition = gameObject.transform.position;
+
+        //get the toward direction
+        towardDir = targetTransform.position - curPosition;
+        towardDir.Normalize();
+
+        rb.AddForce(towardDir* thrust);
+        */
+
+        
+        //get the current position
+        curPosition = gameObject.transform.position;
 
 		//get the current velocity
 		curDir = gameObject.GetComponent<Rigidbody> ().velocity;
-		curDir.Normalize ();
-		Debug.Log ("current dir: " + curDir);
 
 		//get the toward velocity
 		towardDir = targetTransform.position - curPosition;
 		towardDir.Normalize();
-		Debug.Log ("toward dir: " + towardDir);
 
 		//interpolate two vectors
-		updatedDir = Vector3.Lerp(curDir, towardDir, 1.0f);
+		updatedDir = Vector3.Lerp(curDir, towardDir, lerpValue);
 		updatedDir.Normalize ();
-		Debug.Log ("updated dir: " + updatedDir);
-		Debug.Log ("delta position: " + updatedDir*speed*Time.deltaTime);
 
 		//change the position
 		gameObject.GetComponent<Rigidbody>().velocity = updatedDir*speed;
-		gameObject.transform.position = curPosition + updatedDir * speed * Time.deltaTime;
+        
 	}
 }
