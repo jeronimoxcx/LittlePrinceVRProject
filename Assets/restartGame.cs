7bd1fb687feacf04d12e8b54a14b539a035b8048
이건선 <goeka329@gmail.com>
2017-12-10 15:46:06 +0900

    transtion ui script modification

---
+++ Assets/restartGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restartGame : MonoBehaviour {
+
+    public GameObject LevelManager;
+
+	// Use this for initialization
+	void ReStart () {
+        LevelManager.setAcitve(false);
+        LevelManager.setActive(true);
+	}
+	
+	
+}
